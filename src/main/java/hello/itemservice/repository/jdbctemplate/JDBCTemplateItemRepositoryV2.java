5628d0c1e067839cee1a0413fbf257065df29532
Yuhwan Kim <yhkim8046@gmail.com>
2024-03-22 23:36:33 +1300

    NamedParameterJdbcTemPlate-version(1)

---
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JDBCTemplateItemRepositoryV2.java
@@ -0,0 +1,112 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.*;
+
+
+/**
+ * NamedParameterJdbcTemplate
+ * SqlParameterSource
+ * -BeanPropertySqlParameterSource
+ * -MapSqlParameterSource
+ * Map
+ */
+@Slf4j
+public class JDBCTemplateItemRepositoryV2 implements ItemRepository {
+
+    //private final JdbcTemplate template;
+    private final NamedParameterJdbcTemplate template;
+
+    public JDBCTemplateItemRepositoryV2(DataSource dataSource) {
+       this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name, price, quantity) values(:itemName,:price,:quantity)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(sql,param,keyHolder);
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name=:itemName, price=:price, quantity=:quantity where id=:id";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("itemName",updateParam.getItemName())
+                .addValue("price",updateParam.getPrice())
+                .addValue("quantity",updateParam.getQuantity())
+                .addValue("id",itemId);
+
+        template.update(sql,param);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id = :id";
+        try{
+            Map<String, Long>param = Map.of("id",id);
+            Item item = template.queryForObject(sql, param, itemRowMapper());
+            return Optional.of(item);
+        }catch(EmptyStackException e){
+            return Optional.empty();
+        }
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+       return BeanPropertyRowMapper.newInstance(Item.class); //카멜지원
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
+        String sql = "select id, item_name, price, quantity from item ";
+
+        //Dynamic query
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= :maxPrice";
+        }
+        log.info("sql={}", sql);
+        return template.query(sql,param, itemRowMapper());
+    }
+}
